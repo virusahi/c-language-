aeaa54dee87d646af63591210cd20612e706128a
sonal2203 <86613784+sonal2203@users.noreply.github.com>
2023-10-01 12:19:24 +0530

    Update Quick_sort.c
    updated

--- Quick_sort.c
+++ Quick_sort.c
@@ -25,7 +25,7 @@ void quicksort(int number[25],int first,int last){
 }
 int main(){
    int i, count, number[25];
-   printf("How many elements are u going to enter?: ");
+   printf("How many elements are youu going to enter?: ");
    scanf("%d",&count);
    printf("Enter %d elements: ", count);
    for(i=0;i<count;i++)
